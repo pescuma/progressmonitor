f9d5b2dd6b69f73d827ff85bd6a2bb00e9783e09
Ricardo Pescuma Domenecci <ricardo@pescuma.org>
2014-03-21 18:31:24 -0300

    Created FlatToHierarchicalProgressMonitor

---
+++ csharp/ProgressMonitor.Test/FlatToHierarchicalProgressMonitorTest.cs
@@ -0,0 +1,122 @@
+﻿using Moq;
+using NUnit.Framework;
+using org.pescuma.progressmonitor.simple;
+
+namespace org.pescuma.progressmonitor
+{
+	[TestFixture]
+	public class FlatToHierarchicalProgressMonitorTest
+	{
+		private Mock<FlatProgressMonitor> flat;
+		private FlatToHierarchicalProgressMonitor progress;
+
+		[SetUp]
+		public void SetUp()
+		{
+			flat = new Mock<FlatProgressMonitor>();
+
+			progress = new FlatToHierarchicalProgressMonitor(flat.Object);
+		}
+
+		[Test]
+		public void TestStart()
+		{
+			progress.Start(1);
+
+			flat.Verify(f => f.SetCurrent(0, 1000, ""));
+		}
+
+		[Test]
+		public void TestFinished()
+		{
+			progress.Start(1);
+			progress.Finished();
+
+			flat.Verify(f => f.SetCurrent(1000, 1000, ""));
+		}
+
+		[Test]
+		public void Test2Steps()
+		{
+			progress.Start(2);
+			progress.NextStep();
+
+			flat.Verify(f => f.SetCurrent(500, 1000, ""));
+		}
+
+		[Test]
+		public void Test2Steps19()
+		{
+			progress.Start(1, 9);
+			progress.NextStep();
+
+			flat.Verify(f => f.SetCurrent(100, 1000, ""));
+		}
+
+		[Test]
+		public void Test2Steps172()
+		{
+			progress.Start(1, 7, 2);
+			progress.NextStep();
+			progress.NextStep();
+
+			flat.Verify(f => f.SetCurrent(800, 1000, ""));
+		}
+
+		[Test]
+		public void TestStepName()
+		{
+			progress.Start(1);
+			progress.SetStepName("A");
+
+			flat.Verify(f => f.SetCurrent(0, 1000, "A"));
+		}
+
+		[Test]
+		public void TestSubStep_Start()
+		{
+			progress.Start(2);
+			progress.CreateSubMonitor()
+				.Start(2);
+
+			flat.Verify(f => f.SetCurrent(0, 1000, ""), Times.Once);
+		}
+
+		[Test]
+		public void TestSubStep_FinishDoesntPropagate()
+		{
+			progress.Start(2);
+
+			var sub = progress.CreateSubMonitor();
+			sub.Start(2);
+			sub.Finished();
+
+			flat.Verify(f => f.SetCurrent(500, 1000, ""), Times.Never);
+		}
+
+		[Test]
+		public void TestSubStep_StepPropagates()
+		{
+			progress.Start(2);
+
+			var sub = progress.CreateSubMonitor();
+			sub.Start(2);
+			sub.NextStep();
+
+			flat.Verify(f => f.SetCurrent(250, 1000, ""));
+		}
+
+		[Test]
+		public void TestSubStep_SetStepNamePropagates()
+		{
+			progress.Start(2);
+			progress.SetStepName("A");
+
+			var sub = progress.CreateSubMonitor();
+			sub.Start(2);
+			sub.SetStepName("B");
+
+			flat.Verify(f => f.SetCurrent(0, 1000, "A - B"));
+		}
+	}
+}
---
+++ csharp/ProgressMonitor/FlatToHierarchicalProgressMonitor.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using org.pescuma.progressmonitor.simple;
+
+namespace org.pescuma.progressmonitor
+{
+	public class FlatToHierarchicalProgressMonitor : ProgressMonitor
+	{
+		private readonly FlatProgressMonitor monitor;
+		private readonly FlatToHierarchicalProgressMonitor parent;
+
+		private int[] steps;
+		private int currentStep;
+		private string currentStepName;
+
+		public FlatToHierarchicalProgressMonitor(FlatProgressMonitor monitor)
+		{
+			this.monitor = monitor;
+		}
+
+		private FlatToHierarchicalProgressMonitor(FlatToHierarchicalProgressMonitor parent, FlatProgressMonitor monitor)
+		{
+			this.parent = parent;
+			this.monitor = monitor;
+		}
+
+		public IDisposable Start(params int[] aSteps)
+		{
+			CheckNotStarted();
+			if (aSteps.Length < 1)
+				throw new ArgumentException();
+			if (aSteps.Any(v => v < 1))
+				throw new ArgumentException();
+
+			if (aSteps.Length == 1)
+			{
+				steps = new int[aSteps[0]];
+				for (var i = 0; i < steps.Length; i++)
+					steps[i] = 1;
+			}
+			else
+			{
+				steps = aSteps;
+			}
+
+			currentStep = 0;
+
+			if (parent == null)
+				Propagate();
+
+			return new ActionDisposable(Finished);
+		}
+
+		public void SetStepName(string stepName)
+		{
+			CheckStarted();
+
+			if (Equals(currentStepName, stepName))
+				return;
+
+			currentStepName = stepName;
+
+			Propagate();
+		}
+
+		public void NextStep(string stepName = null)
+		{
+			CheckStarted();
+
+			if (currentStep + 1 >= steps.Length)
+				throw new ArgumentException();
+
+			currentStep = currentStep + 1;
+			currentStepName = stepName;
+
+			Propagate();
+		}
+
+		public void Finished()
+		{
+			CheckStarted();
+
+			steps = null;
+
+			if (parent == null)
+				Output(1, "");
+		}
+
+		public ProgressMonitor CreateSubMonitor()
+		{
+			CheckStarted();
+
+			return new FlatToHierarchicalProgressMonitor(this, monitor);
+		}
+
+		private void Propagate()
+		{
+			var percentage = steps.Take(currentStep)
+				.Sum() / (double) steps.Sum();
+			var name = GetFullStepName();
+
+			if (parent == null)
+				Output(percentage, name);
+			else
+				parent.OnChildChange(percentage, name);
+		}
+
+		private void OnChildChange(double percentage, string name)
+		{
+			percentage = (steps.Take(currentStep)
+				.Sum() + steps[currentStep] * percentage) / steps.Sum();
+
+			if (parent == null)
+				Output(percentage, name);
+			else
+				parent.OnChildChange(percentage, name);
+		}
+
+		private void Output(double percentage, string name)
+		{
+			monitor.SetCurrent((int) Math.Round(percentage * 1000), 1000, name);
+		}
+
+		private string GetFullStepName()
+		{
+			var names = new List<string>();
+			var x = this;
+			do
+			{
+				if (x.currentStepName != null)
+					names.Add(x.currentStepName);
+
+				x = x.parent;
+			} while (x != null);
+
+			names.Reverse();
+
+			return string.Join(" - ", names);
+		}
+
+		private void CheckNotStarted()
+		{
+			if (steps != null)
+				throw new InvalidOperationException("Already started");
+		}
+
+		private void CheckStarted()
+		{
+			if (steps == null)
+				throw new InvalidOperationException("Not started or already finished");
+		}
+
+		private class ActionDisposable : IDisposable
+		{
+			private readonly Action dispose;
+
+			public ActionDisposable(Action dispose)
+			{
+				this.dispose = dispose;
+			}
+
+			public void Dispose()
+			{
+				dispose();
+			}
+		}
+	}
+}
