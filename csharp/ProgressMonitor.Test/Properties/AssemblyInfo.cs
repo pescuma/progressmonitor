308e47de1be2502893a467b97b3238c4566bc377
Ricardo Pescuma Domenecci <ricardo@pescuma.org>
2014-03-21 16:25:54 -0300

    Created ProgressMonitor.Test

--- csharp/ProgressMonitor.Test/Properties/AssemblyInfo.cs
+++ csharp/ProgressMonitor.Test/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("ProgressMonitor")]
+[assembly: AssemblyTitle("ProgressMonitor.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ProgressMonitor")]
+[assembly: AssemblyProduct("ProgressMonitor.Test")]
 [assembly: AssemblyCopyright("Copyright © Ricardo Pescuma Domenecci 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -22,7 +22,7 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
-[assembly: Guid("70c6334b-71a8-436b-a459-d23cc5f0e78f")]
+[assembly: Guid("710acf41-db8b-4ef2-ade5-a6cbcf5560bc")]
 
 // Version information for an assembly consists of the following four values:
 //
